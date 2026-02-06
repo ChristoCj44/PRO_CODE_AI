f4a786e258f88b0ccc3254cac309c00f63ec1326
ChristoCj44 <knock4christo2002@gmail.com>
2026-02-06 17:11:17 +0530

    Fix Vercel routing

--- frontend/script.js
+++ frontend/script.js
@@ -71,7 +71,7 @@ async function runCode() {
     const code = editor.getValue();
 
     try {
-        const response = await fetch('/execute', {
+        const response = await fetch('/api/execute', {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({ language: 'python', code })
